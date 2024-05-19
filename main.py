24bce82212c8a353c8f9901c13fe86e56bddc839
NK01T3S <Sosnovskiy_nv@mail.ru>
2024-05-19 21:20:38 +0300

    Modification of the program window. A function for placing a window in the center of the screen.

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
 from tkinter import *
 
-window = Tk()
-window.mainloop()
+def setup_window(root):
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    window_width = 900
+    window_height = 500
+    x = (screen_width // 2) - (window_width // 2)
+    y = (screen_height // 2) - (window_height // 2)
+    root.geometry(f'{window_width}x{window_height}+{x}+{y}')
+
+class Playfield:
+    def __init__(self):
+        self._create_ui()
+        setup_window(self.root)
+
+    def _create_ui(self):
+        self.root = Tk()
+        self.canvas = Canvas(self.root, width=900, height=500, bg="black")
+        self.root.resizable(width=False, height=False)
+        self.root.update_idletasks()
+
+    def start(self):
+        self.root.mainloop()
+
+
+if __name__ == '__main__':
+    p = Playfield()
+    p.start()
