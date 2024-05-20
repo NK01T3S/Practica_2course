f9e9012b78da564040fd6601340aaed39ed4e7cc
NK01T3S <Sosnovskiy_nv@mail.ru>
2024-05-20 23:12:57 +0300

    Creation of game rackets/platforms.

--- main.py
+++ main.py
@@ -19,9 +19,33 @@ class Playfield:
         self.canvas = Canvas(self.root, width=900, height=500, bg="black")
         self.root.resizable(width=False, height=False)
         self.root.update_idletasks()
+        self.canvas.pack()
+        self.p1 = Paddle(self.canvas, "p1", "yellow", 30, 250, 30, 420, 30, 470)
+        self.p2 = Paddle(self.canvas, "p2", "blue", 870, 250, 480, 870, 30, 470)
 
     def start(self):
         self.root.mainloop()
+
+class Paddle:
+    def __init__(self, canvas, tag, color, x=0, y=0, left_boundary=0, right_boundary=900, top_boundary=0, bottom_boundary=500):
+        self.canvas = canvas
+        self.tag = tag
+        self.x = x
+        self.y = y
+        self.color = color
+        self.left_boundary = left_boundary
+        self.right_boundary = right_boundary
+        self.top_boundary = top_boundary
+        self.bottom_boundary = bottom_boundary
+        self.redraw()
+
+    def redraw(self):
+        x0 = self.x - 30
+        x1 = self.x + 30
+        y0 = self.y - 30
+        y1 = self.y + 30
+        self.canvas.delete(self.tag)
+        self.canvas.create_rectangle(x0, y0, x1, y1, tags=self.tag, fill=self.color)
 
 
 if __name__ == '__main__':
